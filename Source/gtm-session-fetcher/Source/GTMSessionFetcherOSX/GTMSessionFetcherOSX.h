177c2d5cf51002498c6108045f8cc324c664c1ae
AndrewKingdom <akingdom@users.noreply.github.com>
2020-09-18 03:34:03 +1000

    pathfix

--- Source/gtm-session-fetcher/Source/GTMSessionFetcherOSX/GTMSessionFetcherOSX.h
+++ Source/gtm-session-fetcher/Source/GTMSessionFetcherOSX/GTMSessionFetcherOSX.h
@@ -2,13 +2,13 @@
 //  GTMSessionFetcherOSX.h
 //
 
-#import "gtm-session-fetcher/Source/GTMSessionFetcher.h"
-#import "gtm-session-fetcher/Source/GTMSessionFetcherService.h"
-#import "gtm-session-fetcher/Source/GTMSessionUploadFetcher.h"
-#import "gtm-session-fetcher/Source/GTMSessionFetcherService.h"
-#import "gtm-session-fetcher/Source/GTMSessionFetcherLogging.h"
-#import "gtm-session-fetcher/Source/GTMGatherInputStream.h"
-#import "gtm-session-fetcher/Source/GTMMIMEDocument.h"
-#import "gtm-session-fetcher/Source/GTMReadMonitorInputStream.h"
+#import "../GTMSessionFetcher.h"
+#import "../GTMSessionFetcherService.h"
+#import "../GTMSessionUploadFetcher.h"
+#import "../GTMSessionFetcherService.h"
+#import "../GTMSessionFetcherLogging.h"
+#import "../GTMGatherInputStream.h"
+#import "../GTMMIMEDocument.h"
+#import "../GTMReadMonitorInputStream.h"
 
 
